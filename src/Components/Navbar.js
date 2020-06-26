f3e72310e2f154b109a06a2cfc75bd9b071cd842
Lucian Boatright <lucian.boatright@gmail.com>
2020-06-26 16:12:24 +0100

    menue shows onclick when window small

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -4,20 +4,36 @@ import "./Navbar.css"
 
 
 class Navbar extends Component {
+
+    state={
+        isOpen: false,
+    }
+
+    menuClick = () => {
+        this.setState({
+            isOpen: !this.state.isOpen,
+        })
+    }
+
+
+
     render(){
         return (
             <nav>
             <div className="logoBtn">
+
                 <div className="logo">
                     <a herf="#"><img src={logo} alt=""/></a>
                 </div>
-              <div className="btn">
+
+              <div className="btn" onClick={this.menuClick}>
                 <div className="bar"></div>
                 <div className="bar"></div>
                 <div className="bar"></div>
               </div>
             </div>
-            <ul>
+
+            <ul className={this.state.isOpen ? "showNav" : "undefined" }>
               <li><a herf="#">Home</a></li>
               <li><a herf="#">Pictures</a></li>
               <li><a herf="#">About</a></li>
