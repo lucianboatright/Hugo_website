1083d218a97cf439c0b27157af0e1b1b7f3ce3e3
Lucian Boatright <lucian.boatright@gmail.com>
2020-07-07 15:50:25 +0100

    about page info, links added

--- src/Components/About.js
+++ src/Components/About.js
@@ -14,16 +14,32 @@ class About extends Component {
             <>
             <div className="about-container">
                 <div className="about_body">
+                    <br></br>
                     <h1>Bristol based lans scape gardener</h1>
-                    
+                    <br></br>
+                    <h3>Small team of expereinced self sufficiant gardeners</h3>
+                    <br></br>
+                    <h3>Based in Bristol but please feel free to contact as we are open to traveling.</h3>
+                    <br></br>
+                    <h3>Feel free to message me and we can discuss the options</h3>
+                    <br></br>
+
                 </div>
                 
                 <div className="gardenLogos">
-                    <div className="RHS_logo">
-                        <Link to="https://www.horticulture.org.uk/"><img src={RHSlogo} alt="" /></Link>
+                    <div className="RHS_box" id="rhs"><h3>HB Gardens and Landscapes are Qualified by the Royal Horticultural Society since 2017</h3>
+                        <div className="RHS_logo">
+                            <Link to="https://www.horticulture.org.uk/"><img src={RHSlogo} alt="" /></Link>
+                        </div>
                     </div>
-                    <div className="hort_logo">
-                        <Link to="https://www.rhs.org.uk/advice/profile?pid=403"><img src={InstHauto} alt="" /></Link>
+                    <div className='holo_block'>
+                        <div></div>
+                    </div>
+                    
+                    <div className="RHS_box" id="hort"><h3>HB Gardens and Landscapes are also registered members of the Chartered Insititute of Horticulture since 2018</h3>
+                        <div className="hort_logo">
+                            <Link to="https://www.rhs.org.uk/advice/profile?pid=403"><img src={InstHauto} alt="" /></Link>
+                        </div>
                     </div>
                 </div>
             </div>
