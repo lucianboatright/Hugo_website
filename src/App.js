7a206f1c815b747599b92a6ca050dca9f35c20a8
Lucian Boatright <lucian.boatright@gmail.com>
2020-07-17 15:20:21 +0100

    added services page

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Navbar from "./Components/Navbar"
 import Home from './Components/Home'
 import About from './Components/About'
 import Contact from './Components/Contact'
+import Services from './Components/Services'
 // import Pictures from './Components/Pictures';
 
 
@@ -17,11 +18,11 @@ function App() {
     <Router>
       <div className="main">
         <Navbar/>
-        <div>some text</div>
         <Switch>
           <Route exact path="/Home" component={Home} />
           <Route exact path="/About" component={About} />
           <Route exact path="/Contact" component={Contact} />
+          <Route exact path="/Services" component={Services} />
           {/* <Route exact path="/Picture" component={Pictures} /> */}
         </Switch>
       </div>
---
+++ src/Components/Services.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom'
+
+import emailLogo from './img/Email_logo.png'
+import phoneLogo from './img/Phone_logo.png'
+import whatsAppLogo from './img/whatsapp.jpg'
+
+import './Styling/Contact.css'
+
+class Services extends Component {
+
+    render(){
+
+        return(
+            <>
+            <div className="Contact_body">
+                <br></br>
+                <h1>Services</h1>
+     
+            </div>
+            </>
+        )
+    }
+}
+
+export default Services;
