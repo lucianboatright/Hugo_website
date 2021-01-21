84a0a47505fe2b4ff4d2e25f487b1fa1809a869a
Lucian Boatright <lucian.boatright@gmail.com>
2021-01-21 15:27:45 +0000

    content on About changes to be more about rarther than what we do

--- src/Components/About.js
+++ src/Components/About.js
@@ -24,10 +24,16 @@ class About extends Component {
                 <div className={about.about_body}>
                     {/* <h2>We cover all of Gloucestershire and Somerset and areas surrounding </h2> */}
                     <h2> HB Designers offer a range of services both specialist and general. </h2>
-                    <h3>Working in scheduled sessions we are able to give seasonal and timely care to the garden and plants, keeping it looking it's best.</h3>
+                    <br></br>
+                    <h3>We have an expereinced team to build and maintian gardens of all types and sizes</h3>
+                    <h3>Our Team is expereinces in working on gardens at all times of the year, weather you are looking to redesign your garden in winter, </h3>
+                    <h3>plant in spring or maintian throughout the summer.</h3>
+                    <br></br>
+                    <h3>HB Design is Qualified by the Royal Horticultural Society and a Registered gardener at the Chartered Insititute of Horticilture</h3>
+                    {/* <h3>Working in scheduled sessions we are able to give seasonal and timely care to the garden and plants, keeping it looking it's best.</h3>
                     <h3>Depending on the season that may be pruning, lawn care and top dressing borders with chippings or mulch.</h3>
                     <h3>Other times that it could be weeding, deadheading, grass cutting and hedge cutting.</h3>
-                    <h3>The work required varies greatly and we are able to provide a complete service. </h3>
+                    <h3>The work required varies greatly and we are able to provide a complete service. </h3> */}
                     <br></br>
                     <h2>We cover all of Bristol, Gloucestershire and Somerset and areas surrounding. </h2>
                     <br></br>
