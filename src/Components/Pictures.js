4a1f8514d180665056cd33d91fb291e78cf5fd1b
Lucian Boatright <lucian.boatright@gmail.com>
2021-02-03 11:16:34 +0000

    added carosel to picture page

--- src/Components/Pictures.js
+++ src/Components/Pictures.js
@@ -1,54 +1,50 @@
-// import React, { Component } from 'react';
+import React, { Component } from 'react';
 // import ReactDOM from 'react-dom';
 // import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 // import { Carousel } from 'react-responsive-carousel';
 
-// import './Styling/Pictures.module.css'
+// import React from "react";
+import ReactDOM from "react-dom";
+import Carousel from "react-elastic-carousel";
+import Item from "./Item";
+import './Styling/Pictures.module.css'
+// import "./styles.css";
+
+const breakPoints = [
+  { width: 1, itemsToShow: 1 },
+  { width: 550, itemsToShow: 2, itemsToScroll: 2 },
+  { width: 768, itemsToShow: 3 },
+  { width: 1200, itemsToShow: 4 }
+];
+
+
+
 
 // import image1 from './ImageGallery/IMG-20201214-WA0000(1).jpg'
 
 
-// import ImageGallery from '../src/ImagrGallery';
 
-
-
-import React, {Component} from 'react';
-
-import Carousel from "react-multi-carousel";
-import "react-multi-carousel/lib/styles.css";
-
-
-const responsive = {
-    superLargeDesktop: {
-      // the naming can be any, depends on you.
-      breakpoint: { max: 4000, min: 3000 },
-      items: 5
-    },
-    desktop: {
-      breakpoint: { max: 3000, min: 1024 },
-      items: 3
-    },
-    tablet: {
-      breakpoint: { max: 1024, min: 464 },
-      items: 2
-    },
-    mobile: {
-      breakpoint: { max: 464, min: 0 },
-      items: 1
-    }
-  };
 
 class Pictures extends Component {
-    render() {
-        return (
-            <Carousel responsive={responsive}>
-                <div>Item 1</div>
-                <div>Item 2</div>
-                <div>Item 3</div>
-                <div>Item 4</div>
-            </Carousel>
-        );
+  
+
+     render() {
+         return (
+            <div className="App">
+                <Carousel breakPoints={breakPoints}>
+                <Item>1</Item>
+                <Item>2</Item>
+                <Item>3</Item>
+                <Item>4</Item>
+                <Item>5</Item>
+                <Item>6</Item>
+                <Item>7</Item>
+                <Item>8</Item>
+                </Carousel>
+            </div>
+         )
+     }
     }
-};
+      
 
 export default Pictures;
